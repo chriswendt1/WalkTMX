261f4081bd07d3e9001cabc8094f836c09ae8b4a
Chris Wendt <chris.wendt@outlook.com>
2023-03-27 16:59:00 -0700

    Failing with ChatGPT code.

--- Program.cs
+++ Program.cs
@@ -1,27 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
 using System.Xml;
+using System.Text.RegularExpressions;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Process TMX");
 
 // Load the TMX file
-XmlDocument xmlDoc = new();
-xmlDoc.Load("path/to/tmx/file");
-
-// Select all TU elements
-XmlNodeList tuNodes = xmlDoc.SelectNodes("//tu");
-
-// Loop through each TU element and apply the method
-foreach (XmlNode tuNode in tuNodes)
+if (args.Length > 0)
 {
-    // Apply your method to the TU element here
-    // For example, you could extract the source and target segments:
-    XmlNode segSource = tuNode.SelectSingleNode("./tuv[@xml:lang='en-US']/seg");
-    string sourceText = segSource.InnerText;
+    ProcessTMX(args[0]);
+    return 0;
+}
+else return 1;
 
-    XmlNode segTarget = tuNode.SelectSingleNode("./tuv[@xml:lang='es-ES']/seg");
-    string targetText = segTarget.InnerText;
+static void ProcessTMX(string fileName)
+{
 
-    // Do something with the source and target segments
-    Console.WriteLine("Source: " + sourceText);
-    Console.WriteLine("Target: " + targetText);
+    XmlDocument xmlDoc = new();
+    xmlDoc.Load(fileName);
+
+    StreamWriter outfile = new(fileName + ".cleanonly.tsv");
+
+    // Select all TU elements
+    XmlNodeList tuNodes = xmlDoc.SelectNodes("//tu");
+
+    // Loop through each TU element and apply the method
+    foreach (XmlNode tuNode in tuNodes)
+    {
+        // Apply your method to the TU element here
+        // For example, you could extract the source and target segments:
+        XmlNode segSource = tuNode.SelectSingleNode("./tuv[@xml:lang='EN-US']/seg");
+        string sourceText = segSource.InnerText;
+
+        XmlNode segTarget = tuNode.SelectSingleNode("./tuv[@xml:lang='ES-ES']/seg");
+        string targetText = segTarget.InnerText;
+
+        string cleanSourceText = RemoveMarkup(sourceText);
+        string cleanTargetText = RemoveMarkup(targetText);
+
+        if ((cleanSourceText != sourceText) || (cleanTargetText != targetText))
+        {
+            outfile.WriteLine($"{cleanSourceText}\t{cleanTargetText}");
+        }
+    }
+
+}
+
+static string RemoveMarkup(string tuv)
+{
+    // regular expression pattern to match XML tags
+    string pattern = @"<[^>]+>";
+
+    // remove all XML tags from the string
+    string plainText = Regex.Replace(tuv, pattern, "");
+
+    // output plain text string
+    return plainText;
+
 }
